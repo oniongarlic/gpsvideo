2b02a470c117a5c5b149079add1bf55f4b5a4f4f
Kaj-Michael Lang <milang@tal.org>
2019-06-04 08:59:45 +0300

    Port over to 1.0 and make it work

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 libtoolize --automake --copy --force
-aclocal-1.11 || aclocal
+aclocal
 mkdir m4
 autoconf --force
 autoheader --force
-automake-1.11 --add-missing --copy --force-missing --foreign || automake --add-missing --copy --force-missing --foreign
+automake --add-missing --copy --force-missing --foreign
--- src/gpsvideo.c
+++ src/gpsvideo.c
@@ -6,18 +6,8 @@
 #include <glib/gstdio.h>
 #include <gst/gst.h>
 
-typedef struct _VideoSrc VideoSrc;
-struct _VideoSrc {
-	GstElement *pipe;
-	GstElement *src;
-	GstElement *queue;
-	GstElement *tee;
-	GstElement *xv;
-	GstElement *sink;
-};
-
-typedef struct _GeoImageSink GeoImageSink;
-struct _GeoImageSink {
+typedef struct _GeoImagePipe GeoImagePipe;
+struct _GeoImagePipe {
 	GstElement *pipe;
 	GstElement *src;
 	GstElement *queue;
@@ -26,18 +16,16 @@ struct _GeoImageSink {
 	GstElement *sink;
 };
 
-VideoSrc vsrc;
-GeoImageSink gis;
+GeoImagePipe gis;
 
 GMainLoop *loop;
 
 #define USE_TAG_SETTER 1
-#define USE_METADATAMUX 0
 
 static gboolean
 set_tag(GstElement *gstjpegenc, gpointer data)
 {
-GeoImageSink *gs=(GeoImageSink *)data;
+GeoImagePipe *gs=(GeoImagePipe *)data;
 GstTagSetter *e=GST_TAG_SETTER(gs->metadata);
 GstTagList *tl;
 GstEvent *te;
@@ -54,84 +42,91 @@ cmt=g_strdup_printf("Geo: %f, %f", lat, lon);
 #ifdef USE_TAG_SETTER
 gst_tag_setter_reset_tags(e);
 gst_tag_setter_add_tags(e,
-	GST_TAG_MERGE_REPLACE, 
-	GST_TAG_COMMENT, cmt, 
+	GST_TAG_MERGE_REPLACE,
+	GST_TAG_COMMENT, cmt,
 	GST_TAG_GEO_LOCATION_LATITUDE, lat,
-	GST_TAG_GEO_LOCATION_LONGITUDE, lon, 
+	GST_TAG_GEO_LOCATION_LONGITUDE, lon,
 	GST_TAG_GEO_LOCATION_NAME, "Testing", NULL);
 #else
 tl=gst_tag_list_new_full(GST_TAG_GEO_LOCATION_LATITUDE, lat,
-	GST_TAG_GEO_LOCATION_LONGITUDE, lon, 
+	GST_TAG_GEO_LOCATION_LONGITUDE, lon,
 	GST_TAG_GEO_LOCATION_NAME, "Testing",
-	GST_TAG_COMMENT, cmt, 
+	GST_TAG_COMMENT, cmt,
 	NULL);
 te=gst_event_new_tag(tl);
 gst_element_send_event(gs->imageenc, te);
 #endif
 
-gst_element_send_event(gs->pipe, gst_event_new_new_segment(FALSE, 1.0, GST_FORMAT_BYTES, 0, -1, 0));
+//gst_element_send_event(gs->pipe, gst_event_new_segment(FALSE, 1.0, GST_FORMAT_BYTES, 0, -1, 0));
 
 return TRUE;
 }
 
 static void
-videosrc()
-{
-vsrc.pipe=gst_pipeline_new("pipeline");
-vsrc.src=gst_element_factory_make("v4l2src", "video");
-vsrc.queue=gst_element_factory_make("queue", "queue");
-vsrc.tee=gst_element_factory_make("tee", "tee");
-vsrc.xv=gst_element_factory_make("xvimagesink", "xv");
-vsrc.sink=gst_element_factory_make("fakesink", "fakesink");
-
-gst_bin_add_many(GST_BIN(vsrc.pipe), vsrc.src, vsrc.queue, vsrc.tee, vsrc.xv, vsrc.sink, NULL);
-
-g_assert(gst_element_link(vsrc.src, vsrc.queue));
-g_assert(gst_element_link(vsrc.queue, vsrc.tee));
-g_assert(gst_element_link(vsrc.tee, vsrc.sink));
-g_assert(gst_element_link(vsrc.tee, vsrc.xv));
-}
-
-static void
-imagesink()
+geoimagepipe()
 {
 gis.pipe=gst_pipeline_new("pipeline");
-gis.imageenc=gst_element_factory_make("jpegenc", "jpeg");
 
-#ifdef USE_METADATAMUX
-gis.metadata=gst_element_factory_make("metadatamux", "meta");
-#else
-gis.metadata=gst_element_factory_make("jifmux", "meta");
-#endif
-
+gis.src=gst_element_factory_make("v4l2src", "video");
 gis.queue=gst_element_factory_make("queue", "queue");
+gis.imageenc=gst_element_factory_make("jpegenc", "jpeg");
+gis.metadata=gst_element_factory_make("jifmux", "meta");
 gis.sink=gst_element_factory_make("filesink", "sink");
 
 gst_bin_add_many(GST_BIN(gis.pipe), gis.src, gis.queue, gis.imageenc, gis.metadata, gis.sink, NULL);
+gst_element_link_many(gis.src, gis.queue, gis.imageenc, gis.metadata, gis.sink, NULL);
 
-g_assert(gst_element_link(gis.src, gis.queue));
-g_assert(gst_element_link(gis.queue, gis.imageenc));
-g_assert(gst_element_link(gis.imageenc, gis.metadata));
-g_assert(gst_element_link(gis.metadata, gis.sink));
-
+g_object_set(gis.src, "num-buffers", 1, NULL);
 g_object_set(gis.imageenc, "quality", 65, NULL);
 g_object_set(gis.sink, "location", "gps.jpg", NULL);
-g_signal_connect(gis.imageenc, "frame-encoded", set_tag, &gis);
+
+set_tag(gis.metadata, &gis);
+}
+
+static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data)
+{
+switch (GST_MESSAGE_TYPE (msg)) {
+	case GST_MESSAGE_EOS:
+		g_print ("End of stream\n");
+		g_main_loop_quit (loop);
+	break;
+	case GST_MESSAGE_WARNING:
+	case GST_MESSAGE_ERROR: {
+		gchar  *debug;
+		GError *error;
+
+		gst_message_parse_error (msg, &error, &debug);
+		g_free (debug);
+
+		g_printerr ("Error: %s\n", error->message);
+		g_error_free (error);
+		g_main_loop_quit (loop);
+	}
+	break;
+}
+
+return TRUE;
 }
 
 gint
 main(gint argc, gchar **argv)
 {
+GstBus *bus;
+int bus_watch_id;
+
 g_type_init();
 gst_init(&argc, &argv);
 
-videosrc();
-imagesink();
+geoimagepipe();
 
-gst_element_set_state(vsrc.pipe, GST_STATE_PLAYING);
+bus = gst_pipeline_get_bus(GST_PIPELINE(gis.pipe));
+bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
+gst_object_unref(bus);
+
+gst_element_set_state(gis.pipe, GST_STATE_PLAYING);
 
 loop=g_main_loop_new(NULL, TRUE);
-g_timeout_add(5000, g_main_loop_quit, loop);
+g_timeout_add(4000, g_main_loop_quit, loop);
 
 g_main_loop_run(loop);
 
